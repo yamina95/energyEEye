06bb9adbf7173ba2fe95916110008e79169a9258
yamina <larbiyamina409@gmail.com>
2026-04-28 23:01:25 +0200

    fix demo button

--- script.js
+++ script.js
@@ -404,6 +404,15 @@ if (demoBtn) {
     loginForm.dispatchEvent(new Event("submit"));
   });
 }
+const demoBtn = document.getElementById("demoLoginBtn");
+
+if (demoBtn) {
+  demoBtn.addEventListener("click", function () {
+    document.getElementById("username").value = "demo";
+    document.getElementById("password").value = "demo123";
+    loginForm.dispatchEvent(new Event("submit"));
+  });
+}
   setupRegisterForm();
   setupDashboardPage();
 });
